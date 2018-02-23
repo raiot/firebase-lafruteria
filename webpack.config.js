1230922e51eb7df096892233ed9d0732e4c3f649
Durazo, Rene <rdurazo@homeaway.com>
2018-02-23 14:40:15 -0600

    trying another webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,64 @@
+const webpack = require('webpack');
+const path = require('path');
+
 module.exports = {
-    entry: "./src/client/index.tsx",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist",
-        publicPath: "http://localhost/dist"
-    },
-
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
-
-    devServer: {
-        watchContentBase: true
-    },
-
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json", ".less", ".png", ".jpg", ".jpeg"]
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.less$/,
-                use: [{
-                    loader: "style-loader" // creates style nodes from JS strings
-                }, {
-                    loader: "css-loader" // translates CSS into CommonJS
-                }, {
-                    loader: "less-loader" // compiles Less to CSS
-                }]
-            },
-            {
-                test: /\.(png|jpg|jpeg|svg)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                      name: '/assets/[name].[ext]'
-                    }
+  entry: path.join(__dirname, 'src', 'client', 'index.tsx'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: "http://localhost/dist"
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json", ".less", ".png", ".jpg", ".jpeg"]
+  },
+  module: {
+    rules: [
+        {
+            test: /\.less$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "less-loader" // compiles Less to CSS
+            }]
+        },
+        {
+            test: /\.(png|jpg|jpeg|svg)$/,
+            use: {
+                loader: 'file-loader',
+                options: {
+                  name: '/assets/[name].[ext]'
                 }
-            },
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+            }
+        },
+        // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+        { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
-        ]
-    },
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
+        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+        { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+    ],
+    loaders: [{
+      test: path.join(__dirname, 'src'),
+      loader: ['babel-loader'],
+      query: {
+        cacheDirectory: 'babel_cache',
+        presets: ['react', 'es2015']
+      }
+    }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      mangle: true,
+      sourcemap: false,
+      beautify: false,
+      dead_code: true
+    })
+  ]
 };
