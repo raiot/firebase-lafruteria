6f27a54815d2175e3a81fe98415cef0d7aa9b28c
Durazo, Rene <rdurazo@homeaway.com>
2018-02-23 17:04:54 -0600

    add https

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: "http://localhost/dist"
+    publicPath: "https://localhost/dist"
   },
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
